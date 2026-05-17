43ea0d54cf770eca730159de8d4f4a7c5ab14281
Celia Pérez <celrezaz@gmail.com>
2026-05-17 22:14:29 +0000

    funciona

--- models/marts/core/fact_objetivos.sql
+++ models/marts/core/fact_objetivos.sql
@@ -3,6 +3,16 @@ with objetivos as (
     from {{ ref('stg_objectives__objetivos') }}
 ),
 
+ventas as (
+    select  
+    mes_id,
+    sk_sales_rep,
+    SUM(ventas_totales) as ventas_totales, 
+    SUM(beneficio_total) as beneficio_total
+    from {{ ref('fact_ventas') }}
+    group by mes_id, sk_sales_rep
+),
+
 dim_rep as (
     select *
     from {{ ref('dim_repventas') }}
@@ -19,13 +29,25 @@ fact as (
         r.sk_sales_rep,
         f.mes_id,          
         o.sales_rep_id,              
-        o.objective_sales
+        o.objective_sales, 
+        v.ventas_totales, 
+        v.beneficio_total,
+        case
+            when o.objective_sales = 0 or o.objective_sales is null then null
+            else round(v.ventas_totales / o.objective_sales * 100, 2)
+        end as pct_cumplimiento
 
     from objetivos o        
     left join dim_rep r
         on  o.sales_rep_id = r.sales_rep_id
     left join dim_fecha f
         on o.mes_id = f.mes_id
+    left join ventas v
+        on o.mes_id = v.mes_id
+        and r.sk_sales_rep = v.sk_sales_rep
 )
 
-select distinct * from fact
+select distinct
+* 
+from fact
+order by objetivo_id
